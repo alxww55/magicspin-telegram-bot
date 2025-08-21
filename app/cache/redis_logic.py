82721c8ee5bd9bc495f56d2b991620e1f095846a
alxww55 <alxww55@icloud.com>
2025-08-21 13:12:14 +0200

    Refactored Redis logic to store sessions

--- app/cache/redis_logic.py
+++ app/cache/redis_logic.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+from datetime import datetime
 import redis.asyncio as redis
 from dotenv import load_dotenv
 
@@ -7,28 +8,43 @@ import app.database.requests as rq
 
 load_dotenv()
 
-# redis_instance =  
 
-async def handle_login_attempts(id: int):
-    async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
-        key = str(id) + "_" + "login"
-        await redis_conn.incr(key)
-        await redis_conn.expire(key, 600)
-        return await redis_conn.get(key)
+class UserSession():
 
-async def get_coins_qty(id: int):
-    async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
-        key = str(id) + "_" + "user_coins"
-        value = await redis_conn.get(key)
-        
-        if not value:
-            coins = await rq.get_coins_by_user_id(id)
-            await redis_conn.set(id, coins)
-            return coins
-        
-        return value
+    def __init__(self, user_id: int | None = None):
+        self.user_id = user_id
 
-async def change_coins_qty(id: int, coins_amount: int):
-    async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
-        key = str(id) + "_" + "user_coins"
-        await redis_conn.set(key, coins_amount)
+    async def init_instance(self):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            exists = await redis_conn.exists(f"user_session:{self.user_id}")
+            if not exists:
+                await redis_conn.hset(f"user_session:{self.user_id}", mapping={
+                    "id": await redis_conn.hincrby(f"user_session:{self.user_id}", "id"),
+                    "user_id": self.user_id,
+                    "login_attempts": 0,
+                    "authorized": 0,
+                    "coins": 1000,
+                    "timestamp": str(datetime.now())
+                })
+
+    async def handle_login_attempts(self):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            await redis_conn.hincrby(f"user_session:{self.user_id}", "login_attempts")
+            await redis_conn.hexpire(f"user_session:{self.user_id}", 600, "login_attempts")
+            return await redis_conn.hget(f"user_session:{self.user_id}", "login_attempts")
+
+    async def authorize_user(self):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            await redis_conn.hset(f"user_session:{self.user_id}", "authorized", "1")
+
+    async def check_authorization_status(self):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            await redis_conn.hget(f"user_session:{self.user_id}", "authorized")
+
+    async def get_coins_qty(self):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            return await redis_conn.hget(f"user_session:{self.user_id}", "coins")
+
+    async def change_coins_qty(self, coins_amount: int):
+        async with redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True) as redis_conn:
+            await redis_conn.hset(f"user_session:{self.user_id}", "coins", coins_amount)
--- app/handlers.py
+++ app/handlers.py
@@ -4,9 +4,10 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from typing import Awaitable
 
 import app.keyboards as kb
-import app.cache.redis_logic as redis
+from app.cache.redis_logic import UserSession
 import app.database.requests as rq
 
 router = Router()
@@ -17,8 +18,14 @@ class AuthorizationStatus(StatesGroup):
     authorized = State()
 
 
+user_session = UserSession()
+
+
 @router.message(CommandStart())
 async def handle_cmd_start(message: Message, state: FSMContext) -> None:
+    user_session.user_id = message.from_user.id
+    await user_session.init_instance()
+    await state.update_data(user_session=user_session)
     # TODO: Check if user in Blacklist (redis)
     black_list = []
     if message.from_user.id in black_list:
@@ -26,11 +33,10 @@ async def handle_cmd_start(message: Message, state: FSMContext) -> None:
     elif await state.get_state() == AuthorizationStatus.authorized:
         await message.answer("Already logged in", reply_markup=kb.main_menu_keyboard)
     else:
-        user_attempts = await redis.handle_login_attempts(message.from_user.id)
+        user_attempts = await user_session.handle_login_attempts()
         if int(user_attempts) >= 5:
             # TODO: Add user to Blacklist
-            # ONLY FOR DEBUG!!!
-            await message.answer("[+] DEBUG added to blacklist\n\n[+] DEBUG reseted login attempts")
+            pass
         await state.set_state(AuthorizationStatus.unathorized)
         await rq.register_unauthorized_users(message.from_user.id)
         keyboard, correct_emoji = await kb.create_captcha_keyboard(message.from_user.id)
@@ -40,11 +46,12 @@ async def handle_cmd_start(message: Message, state: FSMContext) -> None:
 @router.callback_query(F.data.startswith("captcha:"))
 async def check_if_human(callback: CallbackQuery, state: FSMContext) -> None:
     _, chosen_emoji, correct, user_id = callback.data.split(":")
-
+    user_session.user_id = callback.from_user.id
     if chosen_emoji == correct:
         await state.set_state(AuthorizationStatus.authorized)
+        await user_session.authorize_user()
         await rq.add_user_to_authorized(int(user_id))
-        await redis.get_coins_qty(int(user_id))
+        await user_session.get_coins_qty()
         await callback.answer(None)
         await callback.message.edit_text("You solved captcha! ✅")
         await callback.message.answer(f"🎰 {html.bold("Magic Spin - Slot machine simulator")}\n\n💸 Win combinations {html.bold("prizes:")}\n\n7️⃣7️⃣7️⃣ = Bid Amount x10\n\n⬜️⬜️⬜️ = Bid Amount x5\n\n🍋🍋🍋 = Bid Amount x2\n\n🍇🍇🍇 = Bid Amount x2\n\n{html.bold("This project is a non-commercial simulation of Telegram’s slot machine dice feature. It has been developed solely for educational and demonstration purposes.")}", parse_mode="html", reply_markup=kb.main_menu_keyboard)
@@ -58,6 +65,7 @@ async def get_bid_amount(callback: CallbackQuery) -> None:
     await callback.answer(None)
     await callback.message.edit_text(f"Choose the Bid Amount from below:", reply_markup=kb.bid_amounts_keyboard)
 
+
 @router.callback_query(F.data == "main:earn")
 async def add_coins_from_main(callback: CallbackQuery) -> None:
     await callback.answer(None)
@@ -65,9 +73,10 @@ async def add_coins_from_main(callback: CallbackQuery) -> None:
 
 
 @router.callback_query(F.data.startswith("bid_amount:"))
-async def send_slotmachine(callback: CallbackQuery) -> None:
+async def send_slotmachine(callback: CallbackQuery, state: FSMContext) -> None:
     amount = int(callback.data.split(":")[1])
-    cached_coins = int(await redis.get_coins_qty(callback.from_user.id))
+    user_session.user_id = callback.from_user.id
+    cached_coins = int(await user_session.get_coins_qty())
     await callback.answer(None)
     if cached_coins > 0 and cached_coins >= amount:
         await callback.message.edit_text(f"You chose {amount} 🪙", reply_markup=None)
@@ -76,42 +85,45 @@ async def send_slotmachine(callback: CallbackQuery) -> None:
             case 64:
                 win = amount * 10
                 new_balance = cached_coins + win
-                await redis.change_coins_qty(callback.from_user.id, new_balance)
+                await user_session.change_coins_qty(new_balance)
                 await asyncio.sleep(2.2)
                 await callback.message.answer(f"💰 {html.bold("JACKPOT")} 💰\n\n{html.bold(f"YOU GOT: {win}")} 🪙\n\nYour ballance: {new_balance}", parse_mode="html", reply_markup=kb.main_menu_keyboard)
             case 43:
                 win = amount * 2
                 new_balance = cached_coins + win
-                await redis.change_coins_qty(callback.from_user.id, new_balance)
+                await user_session.change_coins_qty(new_balance)
                 await asyncio.sleep(2.2)
                 await callback.message.answer(f"💰 {html.bold("WIN")} 💰\n\n{html.bold(f"YOU GOT: {win}")} 🪙\n\nYour ballance: {new_balance}", parse_mode="html", reply_markup=kb.main_menu_keyboard)
             case 22:
                 win = amount * 2
                 new_balance = cached_coins + win
-                await redis.change_coins_qty(callback.from_user.id, new_balance)
+                await user_session.change_coins_qty(new_balance)
                 await asyncio.sleep(2.2)
                 await callback.message.answer(f"💰 {html.bold("WIN")} 💰\n\n{html.bold(f"YOU GOT: {win}")} 🪙\n\nYour ballance: {new_balance}", parse_mode="html", reply_markup=kb.main_menu_keyboard)
             case 1:
                 win = amount * 5
                 new_balance = cached_coins + win
-                await redis.change_coins_qty(callback.from_user.id, new_balance)
+                await user_session.change_coins_qty(new_balance)
                 await asyncio.sleep(2.2)
                 await callback.message.answer(f"💰 {html.bold("WIN")} 💰\n\n{html.bold(f"YOU GOT: {win}")} 🪙\n\nYour ballance: {new_balance}", parse_mode="html", reply_markup=kb.main_menu_keyboard)
             case _:
                 new_balance = cached_coins - amount
-                await redis.change_coins_qty(callback.from_user.id, new_balance)
+                await user_session.change_coins_qty(new_balance)
                 await asyncio.sleep(2.2)
                 await callback.message.answer(f"😟 {html.bold("Not this time! Try again and WIN!")}\n\nYour ballance: {new_balance}\n\nTap Earn if you runned out of coins", parse_mode="html", reply_markup=kb.main_menu_keyboard)
     else:
         await callback.message.answer(f"😟 {html.bold("You ran out of coins!")} Add some: ", parse_mode=html, reply_markup=kb.add_coins_keyboard)
 
+
 @router.callback_query(F.data.startswith("add_coins"))
-async def add_coins_from_spin(callback: CallbackQuery) -> None:
+async def add_coins_from_spin(callback: CallbackQuery, state: FSMContext) -> None:
     amount = int(callback.data.split(":")[1])
-    cached_coins = int(await redis.get_coins_qty(callback.from_user.id))
+    user_session.user_id = callback.from_user.id
+    cached_coins = int(await user_session.get_coins_qty())
     await callback.answer(None)
-    await redis.change_coins_qty(callback.from_user.id, (cached_coins + amount))
+    await user_session.change_coins_qty((cached_coins + amount))
     await callback.message.edit_text(f"Your ballance is {cached_coins + amount} 🪙\n\nChoose an action from below:", reply_markup=kb.main_menu_keyboard)
+
 
 @router.callback_query(F.data == "cancel")
 async def go_to_main_from_bid_menu(callback: CallbackQuery):
